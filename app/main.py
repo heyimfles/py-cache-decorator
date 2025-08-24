ccbcc64b186b26e4abea99cd3b4eaf68614352e7
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-08-24 18:37:27 +0200

    first solution

--- app/main.py
+++ app/main.py
@@ -2,5 +2,34 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    cache_dict = {}
+    def wrapper(*args, **kwargs):
+        if func(*args, **kwargs) in cache_dict.values():
+            print("Getting from cache")
+            return
+        else:
+            cache_dict[args] = func(*args, **kwargs)
+            print("Calculating new result")
+    return wrapper
+
+@cache
+def long_time_func(a: int, b: int, c: int) -> int:
+    return (a ** b ** c) % (a * c)
+
+@cache
+def long_time_func_2(n_tuple: tuple, power: int) -> int:
+    return [number ** power for number in n_tuple]
+
+long_time_func(1, 2, 3)
+long_time_func(2, 2, 3)
+long_time_func_2((5, 6, 7), 5)
+long_time_func(1, 2, 3)
+long_time_func_2((5, 6, 7), 10)
+long_time_func_2((5, 6, 7), 10)
+
+# Calculating new result
+# Calculating new result
+# Calculating new result
+# Getting from cache
+# Calculating new result
+# Getting from cache
